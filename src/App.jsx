873cac51b236c2366173069def7600b5e6e9ec26
Zehra Ecer <zehraecer1996@hotmail.com>
2024-06-02 12:52:42 +0300

    ekleme yapıldı

--- src/App.jsx
+++ src/App.jsx
@@ -17,21 +17,25 @@ function App() {
 
 
 
-  useLayoutEffect(() => {
-    console.log("zehra ecer");
-  }, [])
+  // useLayoutEffect(() => {
+  //   console.log("zehra ecer");
+  // }, [])
 
-
+  console.log(selectedYear);
   return (
     <>
       <div>
-        <select name="cars" id="cars" style={{ padding: "10px 70px" }}>
-          <option value="2011">2011</option>
-          <option value="2012">2012</option>
-          <option value="2013">2013</option>
-          <option value="2014">2014</option>
-          <option value="2015">2015</option>
-          <option value="2016">2016</option>
+        <select className='option' name="cars" id="cars" style={{ padding: "10px 70px" }} value={selectedYear} onChange={(e) => setSelectedYear(e.target.value)}>
+          <option className='option' value="2011">2011</option>
+          <option className='option' value="2012">2012</option>
+          <option className='option' value="2013">2013</option>
+          <option className='option' value="2014">2014</option>
+          <option className='option' value="2015">2015</option>
+          <option className='option' value="2016">2016</option>
+          <option className='option' value="2017">2017</option>
+          <option className='option' value="2018">2018</option>
+          <option className='option' value="2019">2019</option>
+
 
         </select>
       </div>
